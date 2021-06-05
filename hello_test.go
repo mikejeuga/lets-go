bf9a4e38d2d5c7795014166e14fb77665895dd14
Mike Jeuga <mikejeuga@gmail.com>
2021-06-05 21:20:49 +0100

    :art: Hello % Add
    "Hello and  Add folders with test"

--- hello_test.go
+++ hello_test.go
@@ -3,11 +3,40 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Mike")
-	want := "Hello, Mike!"
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
 	}
+
+	t.Run("say 'Hello, Mike!' when the string 'Mike' is supplied", func(t *testing.T) {
+		got := Hello("Mike", "English")
+		want := "Hello, Mike!"
+		assertCorrectMessage(t, got, want)
+
+	})
+
+	t.Run("say 'Hello, world' when an empty strign is supplied", func(t *testing.T) {
+		got := Hello("", "English")
+		want := "Hello, world!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Michael", "Spanish")
+		want := "Hola, Michael!"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Michael", "French")
+		want := "Bonjour, Michael!"
+
+		assertCorrectMessage(t, got, want)
+	})
 
 }
---
+++ integers/add.go
@@ -0,0 +1,7 @@
+package integers
+
+//Add 2 integers a get the sum as a result of calling the below function
+
+func Add(first, second int) int {
+	return first + second
+}
